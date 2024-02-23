fae0106a60bb442de2fded055ed09cc69c7d4e61
Gukkon <shaunhomer1@hotmail.com>
2024-02-23 18:15:53 +0000

    stripe stuff

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OdBfcDEtpkLfodHl1rRpWIylfAVbCfsaEi1l3N3UNdzgOv2PFyaYPCewWpUrYJAdc4TrjY7awbEWAcru2mLZjmE00vhgMADII',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
